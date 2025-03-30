63988a3056bde4c2405318d6ed195a06ea17c4d0
Piquin-Dylan <dylan.piquin@student.hepl.be>
2025-03-30 14:12:14 +0200

    First commit

---
+++ portfolio/index.php
@@ -0,0 +1,4 @@
+<?php get_header(); ?>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/ressources/reset.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/ressources/styles.css">
+
